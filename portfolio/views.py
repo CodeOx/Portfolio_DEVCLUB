5236a763770e5f40d8fff2fc02a699948108b4de
Chetan Mittal <mittalchetan98@gmail.com>
2017-05-22 10:53:01 +0530

    added contact form

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.mail import send_mail
+from portfolio.forms import ContactForm
 
 def index(request):
     return render(request, 'index.html')
@@ -8,4 +10,20 @@ def about_me(request):
     return render(request, 'about_me.html')
 
 def message(request):
-    return render(request, 'message.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            send_mail(
+                cd['subject'],
+                cd['message'],
+                cd.get('email', 'noreply@example.com'),
+                ['mittal.chetan@yahoo.com'],
+            )
+            return HttpResponseRedirect('/contact/thanks/')
+    else:
+        form = ContactForm()
+    return render(request, 'message.html', {'form':
+form})
+
+    
