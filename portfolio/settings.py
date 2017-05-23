dfd94ad24f79eb2ba4f98efb5ce4cbd7803909a2
Chetan Mittal <mittalchetan98@gmail.com>
2017-05-23 19:15:04 +0530

    mede changes in settings.py for staticfiles

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -125,6 +125,7 @@ STATICFILES_DIRS = (
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 import dj_database_url
